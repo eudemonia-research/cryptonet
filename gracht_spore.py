5e31ebcb9fded4727cab3c7605dc532440dd3d27
Max Kaye <m@xk.io>
2014-04-02 09:42:34 +1100

    exit cleanly

--- gracht_spore.py
+++ gracht_spore.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 ''' What will eventually become the pre-reference Gracht client.
 Haskell client coming later. '''
 
-import time
+import time, argparse
 
 from spore import Spore
 from datastructs import *
@@ -13,11 +13,51 @@ from database import *
 
 intros = {}
 
+# GENESIS BLOCK
+tree = HashTree([
+	BANT("9ebac4128a78623a631834c395c1e044a5bb9bda46e49bbb7428e1a1ec3edc1a", True), 
+	BANT("9ebac4128a78623a631834c395c1e044a5bb9bda46e49bbb7428e1a1ec3edc1a", True), 
+	BANT("193f65c9e4e7b8b92d082784344fad9e732499bc1e7c63f89ae61832cccb7ccc", True), 
+	BANT("193f65c9e4e7b8b92d082784344fad9e732499bc1e7c63f89ae61832cccceb9d", True)
+	])
+chaindata = Chaindata([
+	BANT("00000001", True), 
+	BANT("00000000", True), 
+	BANT("ffffff02", True), 
+	BANT("000000000000", True), 
+	BANT("0000533a5be2", True), 
+	BANT("00000000", True), 
+	BANT("0000000000000000000000000000000000000000000000000000000000000000", True), 
+	BANT("0000000000000000000000000000000000000000000000000000000000000000", True)
+	])
+genesisblock = (tree, chaindata)
+
+
+
+
+config = {
+	'host': '0.0.0.0',
+	'port': 32555
+}
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-port', nargs=1, default=32555, type=int, help='port for node to bind to')
+parser.add_argument('-addnode', nargs=1, default='', type=str, help='node to connect to non-exclusively. Format xx.xx.xx.xx:yyyy')
+parser.add_argument('-genesis', nargs=1, default=BANT(), type=BANT, help='genesis block in hex')
+args = parser.parse_args()
+
+config['port'] = args.port
+seeds = []
+if args.addnode != '':
+	h,p = args.addnode.split(':')
+	seeds.append((h,p))
+
+
 db = Database()
-gpdht = GPDHTChain(db=db)
+gpdht = GPDHTChain(db=db, genesisBlock=genesisblock)
 
 
-gracht = Spore()
+gracht = Spore(seeds=seeds, address=(config['host'], config['port']))
 
 #gracht.set_recvieve_decode(RLP_DESERIALIZE)
 #gracht.set_send_encode(RLP_SERIALIZE)
@@ -25,6 +65,7 @@ gracht = Spore()
 #@gracht.on_connect
 @gracht.handler
 def intro(node, payload):
+	payload = RLP_DESERIALIZE(payload)
 	if node in intros:
 		return None
 	intros[node] = payload
@@ -32,6 +73,7 @@ def intro(node, payload):
 
 @gracht.handler
 def blocks(node, payload):
+	payload = RLP_DESERIALIZE(payload)
 	debug('blocks : %s' % repr(payload))
 	for block in payload:
 		# [[hashtree],[header],[uncleslist]]
@@ -56,7 +98,7 @@ def blocks(node, payload):
 	
 @gracht.handler
 def requestblocks(node, payload):
-	pass
+	payload = RLP_DESERIALIZE(payload)
 	
 	
 @gracht.handler
@@ -104,6 +146,8 @@ def outro(node, payload):
 
 gracht.start()
 
-while True:
-	
-	time.sleep(0.1)
+try:
+	while True:
+		time.sleep(0.1)
+except KeyboardInterrupt:
+	gracht.stop()
