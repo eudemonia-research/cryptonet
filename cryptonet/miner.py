43b57cac2c4fbc09c056ac25b70daad709f10658
Max Kaye <m@xk.io>
2014-04-10 12:59:44 +1000

    Add miner

---
+++ cryptonet/miner.py
@@ -0,0 +1,45 @@
+import threading
+import time
+
+class Miner:
+    def __init__(self, chain, seeknbuild):
+        self._shutdown = False
+        self._restart = False
+        self.threads = [threading.Thread(target=self.mine)]
+        self.chain = chain
+        self.chain.setMiner(self)
+        self.seeknbuild = seeknbuild
+        
+    def run(self):
+        for t in self.threads:
+            t.start()
+        
+    def shutdown(self):
+        self._shutdown = True
+        for t in self.threads:
+            t.join()
+        
+    def restart(self):
+        self._restart = True
+        
+    def mine(self):
+        while not self._shutdown:
+            block = self.chain.head.getCandidate(self.chain)
+            count = 0
+            print('miner restarting')
+            while not self._shutdown and not self._restart:
+                count += 1
+                block.incrementNonce()
+                if block.validPoW():
+                    break
+                if count % 100000 == 0:
+                    self._restart = True
+            if self._shutdown: break
+            if self._restart: 
+                self._restart = False
+                time.sleep(0.01)
+                continue
+            print('Miner: Found Soln : %064x' % block.getHash())
+            self.seeknbuild.addBlock(block)
+            while not self.chain.hasBlockhash(block.getHash()):
+                time.sleep(0.1)
