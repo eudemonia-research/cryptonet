8c252c23f42fb70c876bd1990740136db21a5432
Max Kaye <m@xk.io>
2014-06-14 19:09:54 +1000

    version bump for pip

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='Cryptonet',
-      version='0.0.3',
+      version='0.0.4',
       description='Blockchain and Cryptonet Framework',
       author='Max Kaye',
       author_email='max@eudemonia.io',
